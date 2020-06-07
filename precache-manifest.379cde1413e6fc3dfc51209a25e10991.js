cfc844e92d4a5a99a650d6bdf916f7aeccb8e652
lennajohnsen <lenna@mit.edu>
2020-06-07 16:32:29 -0400

    Updates

---
+++ precache-manifest.379cde1413e6fc3dfc51209a25e10991.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4e36e2211175919d7c67498b12ef1b8",
+    "url": "/annie/index.html"
+  },
+  {
+    "revision": "cd33e9c6c3eab2ec981f",
+    "url": "/annie/static/css/main.50e8a4e9.chunk.css"
+  },
+  {
+    "revision": "8c2f098e288f7641369b",
+    "url": "/annie/static/js/2.4bbab625.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/annie/static/js/2.4bbab625.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cd33e9c6c3eab2ec981f",
+    "url": "/annie/static/js/main.4fb242a1.chunk.js"
+  },
+  {
+    "revision": "a3fe865fb55f343430a0",
+    "url": "/annie/static/js/runtime-main.1b107090.js"
+  },
+  {
+    "revision": "0d918ebe516cbcf66b8330f0373e1bbd",
+    "url": "/annie/static/media/coaster-texture.0d918ebe.jpg"
+  },
+  {
+    "revision": "f5d147f8ff08f185f203f05538812b89",
+    "url": "/annie/static/media/drawn-outline-01.f5d147f8.png"
+  },
+  {
+    "revision": "350b110afddb2a3dc636a3c4fa17f4f5",
+    "url": "/annie/static/media/jojo.350b110a.jpg"
+  }
+]);
