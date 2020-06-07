7937ad9596793ccb1f5f0cf43207511e769fb1ae
lennajohnsen <lenna@mit.edu>
2020-06-07 16:09:15 -0400

    final

--- src/components/PromptsDatabase.js
+++ src/components/PromptsDatabase.js
@@ -1,7 +1,7 @@
 const prompts = [
    
     {"key":0 ,"value":"how often should you feed a sourdough starter?", "answer": "literally whatever annie says she is :queen:"},
-    {"key":1 ,"value":"spell the name of the alcaldía where you can rent colorful boats?", "answer": "Xochimilco"},
+    {"key":1 ,"value":"spell the name of the alcaldía famous for colorful boats", "answer": "Xochimilco"},
     {"key":2 ,"value":"what's Bad Bunny's full name?", "answer": "Benito Antonio Martínez Ocasio"},
     {"key":3 ,"value":"what's the best star sign?", "answer": "...gemini"},
     {"key":4 ,"value":"where did the Oakland As play until 2019?", "answer": "Oakland Coliseum"},
